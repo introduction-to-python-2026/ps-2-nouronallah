50babb176848b479a08a9ecf1cabed3c9e07bb83
nouronallah <nour.onallah1234@gmail.com>
2025-11-02 08:23:37 -0800

    Fix return values in find_max_number function

--- math_utils.py
+++ math_utils.py
@@ -1,8 +1,8 @@
 def find_max_number(num1, num2, num3):
     if num1 > num2 and num1 > num3:
-      return ("num 1")
+      return (num 1)
     elif num2> num3 and num2 > num3:
-      return ("num 2")
+      return (num 2)
     else:
       return (num3)
 
