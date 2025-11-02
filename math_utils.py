ed7ef0b48673818a971bfa953e91796b4855bb2b
nouronallah <nour.onallah1234@gmail.com>
2025-11-02 08:16:43 -0800

    Fix indentation and variable naming in math_utils
    Fix indentation and correct variable names in mean calculations.

--- math_utils.py
+++ math_utils.py
@@ -4,13 +4,13 @@ def find_max_number(num1, num2, num3):
     elif num2> num3 and num2 > num3:
       print("num 2")
     else:
-      print(num3) 
+      print(num3)
 
 def find_mean(num1, num2, num3):
     find_mean = (num1 + num2 + num3) / 3
-return find_mean
+    return find_mean
 
 def find_mean_std(num1, num2, num3):
     mean = find_mean(num1, num2, num3)
     find_mean_std = (((num1 - mean_value)**2 + (num2 - mean_value)**2 + (num3 - mean_value)**2) / 3) **0.5
-return find_mean_std, mean
+    return find_mean_std, mean
