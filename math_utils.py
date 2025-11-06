21b1ccf144a2cfadcab20260862ffa3c3f96806a
nouronallah <nour.onallah1234@gmail.com>
2025-11-05 22:43:54 -0800

    Fix syntax errors in find_max_number and find_mean_std

--- math_utils.py
+++ math_utils.py
@@ -1,10 +1,10 @@
 def find_max_number(num1, num2, num3):
-    if num1 >= num2 and num1 >= num3:
-      return (num 1)
-    elif num2 >= num1 and num2 >= num3:
-      return (num 2)
-    else:
-      return (num3)
+ if num1 >= num2 and num1 >= num3:
+  return (num1)
+ elif num2 >= num1 and num2 >= num3:
+  return (num2)
+ else:
+  return (num3)
 
 def find_mean(num1, num2, num3):
     x = (num1 + num2 + num3) / 3
@@ -12,5 +12,5 @@ def find_mean(num1, num2, num3):
 
 def find_mean_std(num1, num2, num3):
     mean = find_mean(num1, num2, num3)
-    mean_std = (((num1 - mean_value)** 2 + (num2 - mean_value)** 2 + (num3 - mean_value)** 2)) / 3)** 0.5 
+    mean_std = (((num1 - mean)** 2 + (num2 - mean)** 2 + (num3 - mean)** 2)) / 3)** 0.5 
     return (mean, mean_std)
